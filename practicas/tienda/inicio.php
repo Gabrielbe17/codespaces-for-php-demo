f75e5903f2298d0fd34e4534e8a46709e92dd15d
Gabrielbe17 <70650366+Gabrielbe17@users.noreply.github.com>
2024-10-20 11:58:15 +0000

    pr6 inicio.php, estilos y formularios

--- practicas/tienda/inicio.php
+++ practicas/tienda/inicio.php
@@ -12,11 +12,28 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienvenido a Mercadona</title>
-    <!-- link css bootrap  -->
-    <link href="https: /cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384- 1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bienvenido a Mercadona</title>
+  <!-- link css bootrap  -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384- 1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <style>
+    *{
+      box-sizing: border-box;
+    }
+    body{
+      display: flex;
+      align-items: center;
+      min-height: 100vh;
+    }
+    .container-fluid{
+      max-width: 75rem;
+      padding: 1rem;
+      margin: 0 auto;
+    }
+
+  </style>
+
 </head>
 <body>
     <div class="container-fluid">
@@ -26,81 +43,60 @@
         </div>
 
         <div class="container">
-            <!-- AQUÍ PUEDES USAR UN FORM DE BOOTSTRAP 5 SENCILLO PARA PEDIR LOS -->
-            <!-- DATOS: -->
-            <!-- NOMBRE -->
-            <!-- APELLIDOS -->
-            <!-- NÚMERO DE TELÉFONO -->
-            <!-- DNI -->
-            <!-- CÓDIGO DE SOCIO -->
-            <!-- CORREO ELECTRÓNICO -->
-            <form class="row g-3 needs-validation" novalidate>
+            <form class="row g-3 needs-validation d-flex flex-column align-items-center" action="" novalidate>
               <div class="col-md-4">
-                <label for="validationCustom01" class="form-label">First name</label>
-                <input type="text" class="form-control" id="validationCustom01" value="Mark" required>
+                <label for="validationCustom01" class="form-label">Nombre: </label>
+                <input type="text" class="form-control" id="validationCustom01" value="" required>
                 <div class="valid-feedback">
                   Looks good!
                 </div>
               </div>
+
               <div class="col-md-4">
-                <label for="validationCustom02" class="form-label">Last name</label>
-                <input type="text" class="form-control" id="validationCustom02" value="Otto" required>
-                <div class="valid-feedback">
-                  Looks good!
-                </div>
+                <label class="form-label" for="typePhone">Número de Teléfono: </label>
+                <input type="tel" id="typePhone" class="form-control" required>
               </div>
+
               <div class="col-md-4">
-                <label for="validationCustomUsername" class="form-label">Username</label>
-                <div class="input-group">
-                  <span class="input-group-text" id="inputGroupPrepend">@</span>
-                  <input type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>
-                  <div class="invalid-feedback">
-                    Please choose a username.
+                <label for="basic-url">URL Foto de Perfil: </label>
+                <div class="input-group mb-3">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text" id="basic-addon3">https://.../img.png</span>
                   </div>
+                  <input type="url" class="form-control" id="basic-url" aria-describedby="basic-addon3" required>
                 </div>
               </div>
-              <div class="col-md-6">
-                <label for="validationCustom03" class="form-label">City</label>
-                <input type="text" class="form-control" id="validationCustom03" required>
-                <div class="invalid-feedback">
-                  Please provide a valid city.
-                </div>
-              </div>
-              <div class="col-md-3">
-                <label for="validationCustom04" class="form-label">State</label>
-                <select class="form-select" id="validationCustom04" required>
-                  <option selected disabled value="">Choose...</option>
-                  <option>...</option>
-                </select>
-                <div class="invalid-feedback">
-                  Please select a valid state.
-                </div>
-              </div>
-              <div class="col-md-3">
-                <label for="validationCustom05" class="form-label">Zip</label>
-                <input type="text" class="form-control" id="validationCustom05" required>
-                <div class="invalid-feedback">
-                  Please provide a valid zip.
-                </div>
-              </div>
-              <div class="col-12">
-                <div class="form-check">
-                  <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
-                  <label class="form-check-label" for="invalidCheck">
-                    Agree to terms and conditions
-                  </label>
-                  <div class="invalid-feedback">
-                    You must agree before submitting.
-                  </div>
-                </div>
-              </div>
-              <div class="col-12">
-                <button class="btn btn-primary" type="submit">Submit form</button>
+
+              <div class="col-md-4 text-center">
+                <button class="btn btn-primary w-100" type="submit">Enviar</button>
               </div>
             </form>
         </div>
     </div>
 
-    <script src="https: /cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script>
+      //validacion con bootstrap
+      // Example starter JavaScript for disabling form submissions if there are invalid fields
+      (function () {
+        'use strict'
+
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.querySelectorAll('.needs-validation')
+
+        // Loop over them and prevent submission
+        Array.prototype.slice.call(forms)
+          .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+              if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+              }
+
+              form.classList.add('was-validated')
+            }, false)
+          })
+      })()
+    </script>
 </body>
 </html>
