760fa6e12a08236674afb11bc2037a1570ef7faf
Gabrielbe17 <70650366+Gabrielbe17@users.noreply.github.com>
2024-10-25 10:00:57 +0000

    ej ruleta cambios en los selects

--- practicas/ruletaAmericana/scripts/main.js
+++ practicas/ruletaAmericana/scripts/main.js
@@ -12,6 +12,7 @@ selectApuesta.addEventListener('change', (e) =>{
     selectPosiblesApuestas.innerHTML = options;
 });
 
+//TODO: FUNCION mostrarPosiblesApuestas
 function mostrarPosiblesApuestas(tipoApuesta){
     let options = '';
     switch (tipoApuesta) {
@@ -19,8 +20,56 @@ function mostrarPosiblesApuestas(tipoApuesta){
             options = `<option>Rojo</option>
             <option>Negro</option>`
             break;
+        case 'Par/Impar':
+            options = `<option>Par</option>
+            <option>Impar</option>`
+            break;
+        case 'Pasa/Falta':
+            options = `<option>Pasa</option>
+            <option>Falta</option>`
+            break;
+        case 'Pleno':
+            for (let i = 0; i < 36; i++) {
+                options += `<option>${i+1}</option>`;
+            }
+            break;
+        case 'Docena':
+            options = `<option>1a docena</option>
+            <option>2a docena</option>
+            <option>3a docena</option>`
+            break;
+        case 'Columna':
+            options = `<option>1a columna</option>
+            <option>2a columna</option>
+            <option>3a columna</option>`
+            break;
+        case 'Dos docenas':
+            options = `<option>1a y 2a dozena</option>
+            <option>2a y 3a dozena</option>`
+            break;
+        case 'Dos columnas':
+            options =`<option>1a y 2a columna</option>
+            <option>2a y 3a columna</option>`
+            break;
+        case 'Seisena':
+            options = `<option></option>
+            <option></option>`
+            break;
+        case 'Cuadro':
+            options = `<option></option>
+            <option></option>`
+            break;
+        case 'Transversal':
+            options = `<option></option>
+            <option></option>`
+            break;
+        case 'Caballo':
+            options = `<option></option>
+            <option></option>`
+            break;
         default:
             break;
     }
     return options;
 }
+
