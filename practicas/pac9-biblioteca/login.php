e690d14d1a285e7d44833c063e6ed7f33cf428a7
Gabrielbe17 <70650366+Gabrielbe17@users.noreply.github.com>
2024-11-14 18:56:34 +0000

    primera aprox login php

--- practicas/pac9-biblioteca/login.php
+++ practicas/pac9-biblioteca/login.php
@@ -7,12 +7,34 @@ $usuarios = [
     ["username" => "reader", "password" => "readerpass", "role" => "lector"]
 ];
 
+$username = $_POST['username'];
+$pass = $_POST['password'];
+$role = null;
+
 // Procesamiento del formulario.
+// Validación de credenciales.    
+function validateUser($username, $pass){
+    global $usuarios;
+    global $role;
 
+    foreach ($usuarios as $usuario) {
+        if ($usuario['username'] === $username && $usuario['password'] === $pass) {
+            $role = $usuario['role'];
+            return true;
+        }
+    }
+    return false;
+}
 
-    // Validación de credenciales.
-   
-  
+if (validateUser($username, $pass)) {
+    $_SESSION['username'] = $_POST['username'];
+    $_SESSION['role'] = $role;
+    $_SESSION['photo'] = $_POST['urlpicture'];
+
+    header('Location: home.php');
+    exit();
+}
+
 
 ?>
 
@@ -22,7 +44,7 @@ $usuarios = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Biblioteca Virtual - Login</title>
-    <link rel="stylesheet" href="style.css"> <!-- Usa el archivo de estilo que acompaña a la estructura de HTML proporcionada -->
+    <link rel="stylesheet" href="./css/styles.css"> <!-- Usa el archivo de estilo que acompaña a la estructura de HTML proporcionada -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
@@ -36,17 +58,17 @@ $usuarios = [
         <div class="signin">
             <div class="content text-center">
                 <h2>Inicia sesión</h2>
-                <form method="xxxxx" action="login.php">
+                <form method="POST" action="#">
                     <div class="inputBox ">
-                        <input class="p-2 m-2" placeholder="Username" type="text" name="" required>
+                        <input class="p-2 m-2" placeholder="Username" type="text" name="username" required>
                        
                     </div>
                     <div class="inputBox ">
-                        <input class="p-2 m-2" placeholder="Password" type="password" name="" required>
+                        <input class="p-2 m-2" placeholder="Password" type="password" name="password" required>
                        
                     </div>
                     <div class="inputBox ">
-                        <input class="p-2 m-2" placeholder="Foto de perfil" type="text" name="" required>
+                        <input class="p-2 m-2" placeholder="Foto de perfil" type="text" name="urlpicture" required>
                        
                     </div>
                     <?php if (!empty($error)) : ?>
@@ -61,7 +83,7 @@ $usuarios = [
         </div>
     </section>
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </body>
 </html>
